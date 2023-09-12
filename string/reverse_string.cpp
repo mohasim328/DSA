f26659f6bf2e1ba3c2b21a94d24099f983a1f147
moahsim328 <mohasim3283@gmail.com>
2023-09-12 12:28:29 +0530

    array practice

---
+++ string/reverse_string.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        // Your code goes here
+          int start =0;
+          int end = str.length() - 1;
+          
+          while(start<end){
+              
+              int temp = str[end];
+              
+              str[end] = str[start];
+              
+              str[start] = temp;
+              
+              end--;
+              start++;
+              
+              
+          }
+          return str;
+
+    }
+};
+
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+    	string s;
+    	cin >> s;
+    	Solution ob;
+    	cout << ob.reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
